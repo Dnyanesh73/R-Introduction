e28e5926d5a290a5ef27398e206ede3c72709f11
Dnyanesh73 <dnyaneshwarsontakke.33@gmail.com>
2025-02-12 22:30:57 +0530

    Add files via upload

---
+++ Introduction to R.R
@@ -0,0 +1,57 @@
+"Hello World!"
+
+'Thank You'
+
+#Hello World!
+
+# to output number 
+5
+
+5 10 10 # Wrong syntax  raise error
+
+# Variable 
+name1 = "amit"
+name1
+Name1
+
+age = 25
+age
+
+x = 21
+x
+age = age + 5
+
+num = "20"
+num
+num = num + 5
+
+4*8
+35/(2+5)
+(4+2)/4
+
+text = "Excellent"
+paste("Excerl is ",text)
+paste("you are an ",text)
+paste("my name is ",name)
+paste ("math is ",text,"subject")
+
+#Assign same value to multiple variables in one line
+var1=var2=var3="Red"
+
+#legal variables name 
+myvar = "Snehal"
+MYVAR = "MANISH"
+MYvar = "ganesh"
+MY_var = "Sakshi"
+myvar2 = "abhi"
+this.year = 2025
+
+#Illegal variables
+2MYvar = "s"
+my var = "s"
+My-var = "s"
+_my_var = "s"
+my@var = "s"
+TRUE = "s"
+
+
